a2f571feee35b175e96fcaa80d74d8b1859df574
Kushalappa C A <kushalappa.ca@Incture.local>
2024-06-15 23:21:13 +0530

    added ui and backend endpts for future reference

--- src/main/java/com/prj/chatapp/constants/UrlConstants.java
+++ src/main/java/com/prj/chatapp/constants/UrlConstants.java
@@ -4,3 +4,9 @@ public class UrlConstants {
 	public static final String UI_ENDPOINT = "http://localhost:3000";
 	public static final String BACKEND_ENDPOINT = "http://localhost:8080";
 }
+
+//remote-ui: https://chat-app-ui-vercel1.vercel.app
+//remote-backend: https://chatapp-zoa1.onrender.com
+
+//local-ui: http://localhost:3000
+//local - backend: http://localhost:8080
